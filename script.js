cebf71e38dbc25a848f867eca891569c2eef1df9
SMCSEAFOOD <afifuddin.tugas@gmail.com>
2026-07-16 22:56:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,87 @@
-document.getElementById("produk").innerHTML=`
+const produk = [
+
+{
+
+nama:"Udang Premium",
+
+harga:90000,
+
+gambar:"images/IMG_5874.jpeg",
+
+emoji:"🦐"
+
+},
+
+{
+
+nama:"Cumi Premium",
+
+harga:90000,
+
+gambar:"images/IMG_5891.jpeg",
+
+emoji:"🦑"
+
+}
+
+];
+
+let html="";
+
+produk.forEach((item,index)=>{
+
+html+=`
 
 <div class="card">
 
-<img src="images/IMG_5874.jpeg">
+<img src="${item.gambar}">
 
-<h2>🦐 Udang Premium</h2>
+<h2>${item.emoji} ${item.nama}</h2>
 
-<h3>Rp90.000 / Kg</h3>
+<h3>Rp${item.harga.toLocaleString("id-ID")} / Kg</h3>
+
+<div class="qty">
+
+<button onclick="kurang(${index})">−</button>
+
+<span id="qty${index}">0</span>
+
+<button onclick="tambah(${index})">+</button>
 
 </div>
 
-<div class="card">
+<button class="pesan">
 
-<img src="images/IMG_5891.jpeg">
+Tambah Keranjang
 
-<h2>🦑 Cumi Premium</h2>
-
-<h3>Rp90.000 / Kg</h3>
+</button>
 
 </div>
 
 `;
+
+});
+
+document.getElementById("produk").innerHTML=html;
+
+let qty=[0,0];
+
+function tambah(i){
+
+qty[i]++;
+
+document.getElementById("qty"+i).innerHTML=qty[i];
+
+}
+
+function kurang(i){
+
+if(qty[i]>0){
+
+qty[i]--;
+
+document.getElementById("qty"+i).innerHTML=qty[i];
+
+}
+
+}
